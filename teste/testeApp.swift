ab5cd58d44529c9753e47f734e08392ec9abc96f
ewrwerewr <werewrewr@gmai.com>
2023-05-24 17:50:04 +0100

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Aluno ISTEC on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
